a1ec0d23459c120859d78336e4d801c1148af22e
arianafc <123421110+arianafc@users.noreply.github.com>
2025-02-28 15:24:11 -0600

    updates

--- index.php
+++ index.php
@@ -21,29 +21,42 @@
         <div class="container d-flex justify-content-between align-items-center top-bar p-2">
         <span><i class="fas fa-map-marker-alt"></i> Cartago, Costa Rica</span>
         <div>
-                <span><a href="" class="fw-bold text-dark">Sign In / Sign Up</a></span>
+                <span><a href="" class="fw-bold text-dark">Iniciar Sesión</a></span>
             </div>
         </div>
     </div>
     <nav class="navbar navbar-expand-lg bg-white shadow-sm">
-    <a class="navbar-brand d-flex align-items-center" href="#">
+    <a class="navbar-brand d-flex align-items-center" href="index.php">
                 <span class=" tituloNav fw-bold"><i class="fas fa-solid fa-truck"></i> DISTRIBUIDORA EL LEGADO</span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
+    
     </nav>
+    <div class="opciones container-fluid p-3">
+    <span ><a href="" class="text-white m-5">Home</a></span>
+        <span ><a href="" class="text-white m-5">Productos</a></span>
+        <span ><a href="" class="text-white m-5">Contáctanos</a></span>
+        <span ><a href=""  class="text-white m-5">Nosotros</a></span>
+        <span ><a href="" class="text-white m-5">Cotizaciones</a></span>
+    </div>
     <main>
         <section>
-    <div class="mainContainer container row w-100">
-        <div class="col">
-        <img src="./img/familia.png" >
+    <div class="mainContainer container-fluid row ">
+        <div class="col p-0 ">
+        <img class="object-fit-lg-contain border rounded imagenHP" src="./img/familia.png" >
         </div>
-        <div class="col">
-
-        </div>
-        
+        <div class="col align-self-center p-5">
+             <h4>LLEVANDO DELICIAS A TU NEGOCIO DESDE 2023</h4>
+             <h1 class="tituloHP">HECHO POR TICOS, <br>PARA TICOS</h1>
+             <span>Producto 100% artesanal</span>
+             <br><br>
+             <span><a href="" class="fw-bold text-dark">¿Quienes Somos?</a></span>
+</div>
     </div>
+    <hr>
+    <h1 class="productosHP text-center">NUESTROS PRODUCTOS</h3>
         </section>
     </main>
 </body>
